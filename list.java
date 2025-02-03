02b1526d6b6b95634d1d79e9e05af071184ac005
Mayankkumar27 <156913727+Mayankkumar27@users.noreply.github.com>
2025-02-03 22:42:14 +0530

    Add files via upload

---
+++ list.java
@@ -0,0 +1,18 @@
+package framework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class list {
+	public static void main(String[] args) {
+		List<String> l = new ArrayList<>();
+		
+		l.add("hi");
+		l.add("I");
+		l.add("am");
+		l.add("Mayank");
+		
+		System.out.println(l);
+	}
+
+}
---
+++ set.java
@@ -0,0 +1,22 @@
+package framework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class set {
+
+	public static void main(String[] args) {
+		Set<String> s = new HashSet<>();
+		
+		s.add("hi");
+		s.add("I");
+		s.add("am");
+		s.add("Mayank");
+		s.add("Kumar");
+		
+		System.out.println(s);
+		
+		
+	}
+
+}
